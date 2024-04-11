40aad6ffd7306836c0273fadc9fc9f0fc9fc6ec2
Arjun Gill <arjungill6ep@gmail.com>
2024-04-11 08:29:34 +0100

    intial commit before adding navbar

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Alert, Image, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{ title: 'Welcome' }}
+          />
+          <Stack.Screen name="About" c
+            style={styles.logo}
+            source={require('./assets/FINH.png')}
+          ></Image>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Let's GO</Text>
+          </TouchableOpacity>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +39,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  logo: {
+    maxHeight: 50,
+    resizeMode: 'contain',
+  },
+  button: {
+    backgroundColor: 'black',
+    padding: 15,
+    marginVertical: 15,
+    marginHorizontal: 16,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: 'white',
   },
 });
