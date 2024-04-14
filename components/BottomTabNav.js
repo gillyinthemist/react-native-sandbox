d64704e45f8c23d0d5cd1e22a5d0d846fed17c85
Arjun Gill <arjungill6ep@gmail.com>
2024-04-14 20:53:12 +0100

    Full wireframe

--- components/BottomTabNav.js
+++ components/BottomTabNav.js
@@ -6,8 +6,32 @@ import Feather from 'react-native-vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
 
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image } from 'react-native';
 import styles from '../styles';
+
+function CustomTabBarButton({ children, onPress }) {
+  return (
+    <TouchableOpacity
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        ...styles.shadow,
+      }}
+      onPress={onPress}
+    >
+      <View
+        style={{
+          width: 70,
+          height: 70,
+          borderRadius: 35,
+          backgroundColor: '#e32f45',
+        }}
+      >
+        {children}
+      </View>
+    </TouchableOpacity>
+  );
+}
 
 export default function BottomTabNav() {
   return (
@@ -26,6 +50,16 @@ export default function BottomTabNav() {
           tabBarShowLabel: false,
           tabBarIcon: ({ color, size }) => (
             <AntDesign name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Middle"
+        component={HomeScreen} // Change this to your middle screen component
+        options={{
+          tabBarShowLabel: false,
+          tabBarButton: (props) => (
+            <CustomTabBarButton {...props}></CustomTabBarButton>
           ),
         }}
       />
